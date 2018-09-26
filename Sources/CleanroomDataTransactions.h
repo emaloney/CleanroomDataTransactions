603ebe60b7d75f65c0aeb10c90c187d2b8676c1e
Nikita Korchagin <nkorchagin@gilt.com>
2018-09-26 19:51:34 +0300

    Add framework public header file

---
+++ Sources/CleanroomDataTransactions.h
@@ -0,0 +1,17 @@
+//
+//  CleanroomDataTransactions.h
+//  CleanroomDataTransactions
+//
+//  Created by Nikita Korchagin on 03/08/2018.
+//  Copyright © 2018 HBC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for CleanroomDataTransactions.
+FOUNDATION_EXPORT double CleanroomDataTransactionsVersionNumber;
+
+//! Project version string for CleanroomDataTransactions.
+FOUNDATION_EXPORT const unsigned char CleanroomDataTransactionsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CleanroomDataTransactions/PublicHeader.h>
