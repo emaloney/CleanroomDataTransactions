10a236eed11427a8ea233c40294dda342f35a0ae
Kevin Sweeney <kevinsweeney1986@gmail.com>
2019-06-27 16:29:16 +0100

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -1,5 +1,18 @@
+// swift-tools-version:4.0
 import PackageDescription
 
 let package = Package(
-    name: "CleanroomDataTransactions"
+    name: "CleanroomDataTransactions",
+    products: [
+        .library(
+            name: "CleanroomDataTransactions",
+            targets: ["CleanroomDataTransactions"]
+        )
+    ],
+    targets: [
+        .target(
+            name: "CleanroomDataTransactions",
+            path: "Sources"
+        )
+    ]
 )
